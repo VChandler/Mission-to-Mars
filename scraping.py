b2fc3da57cacc76c4995acbbb5f82bf80ba81d13
Vince Chandler <vincechandler@gmail.com>
2021-10-18 00:13:30 -0500

    Deliverable 2 added

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemi_data(browser)
     }
 
     # Stop webdriver and return data
@@ -101,3 +102,50 @@ if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
+def hemi_data(browser):
+
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+    results = hemi_soup.find_all('div', class_='item')
+
+    # Loop thru results to get image and titles
+    for result in results:
+        hemispheres = {}
+        
+        # scrape the image thumbnail url
+        image_thumb = result.find('a').get('href')
+        new_url = f'{url}{image_thumb}'
+        
+        # Visit site with full image link
+        browser.visit(new_url)
+        html = browser.html
+        img_result = soup(html, 'html.parser')
+        
+        # Find link to full jpg file
+        partial_img_tag = img_result.find(string='Sample')
+        partial_img_url = partial_img_tag.find_parent().get('href')
+        full_img_url = f'{url}{partial_img_url}'
+        browser.back()
+        
+        # scrape the title
+        hemi_title = result.find('h3').text
+        
+        # Add values to dictionary
+        hemispheres = {
+            'img_url': full_img_url,
+            'title': hemi_title
+        }
+        
+        # Add dictionary to hemisphere_image_urls list
+        hemisphere_image_urls.append(hemispheres.copy())
+
+    return hemisphere_image_urls
